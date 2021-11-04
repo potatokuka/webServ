e4cf596ada74e0978a0ecf31ca76b552204a7db0
sanderlegit <sanderverheijen@outlook.com>
2021-11-04 15:28:33 +0100

    util print_debug fixed

--- Util/main.cpp
+++ Util/main.cpp
@@ -6,9 +6,12 @@
 /*   By: dries <sanderlegit@gmail.com>                8!   .dWb.   !8         */
 /*                                                    Y8 .e* 8 *e. 8P         */
 /*   Created: 2021/10/07 14:46:50 by dries             *8*   8   *8*          */
-/*   Updated: 2021/11/03 16:04:46 by dries               **ee8ee**            */
+/*   Updated: 2021/11/04 15:26:26 by dries               **ee8ee**            */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Util.hpp"
+
 int		main(void) {
+	print_debug("debug", "success", 10);
 }
--- Util/src/Util.hpp
+++ Util/src/Util.hpp
@@ -6,7 +6,7 @@
 /*   By: dries <sanderlegit@gmail.com>                8!   .dWb.   !8         */
 /*                                                    Y8 .e* 8 *e. 8P         */
 /*   Created: 2021/10/07 14:36:47 by dries             *8*   8   *8*          */
-/*   Updated: 2021/11/03 16:42:19 by dries               **ee8ee**            */
+/*   Updated: 2021/11/04 15:27:05 by dries               **ee8ee**            */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,5 +53,37 @@
 std::string		skip_whitespace(std::string str);
 
 template<class debug_type>
-void 			print_debug(std::string message, debug_type debug, int w);
+void 			print_debug(std::string message, debug_type debug, int w) {
+	if (DEBUG) {
+		std::stringstream 	buf; 
+		std::string			str; 
+		char				escs[2] = {'\r', '\n'}; 
+		std::string			litr[2] = {"\\r", "\\n"}; 
+		bool				flag; 
+		/*Assemble string to print */
+		str = message;
+		buf << str; 
+		for (int i = str.length(); i < w; i++) { 
+			buf << " "; 
+		} 
+		buf << "'" << debug << "'"; 
+		str = buf.str(); 
+		/*Print string */
+		for (unsigned int i = 0; i < str.length(); i++) { 
+			/*Literal print escaped characters */
+			flag = false; 
+			for (int c = 0; c < 2; c++) { 
+				if(str[i] == escs[c]) { 
+					std::cout << litr[c]; 
+					flag = true; 
+					break; 
+				} 
+			} 
+			/*Print non-escaped characters */
+			if (!flag) 
+				std::cout << str[i]; 
+		} 
+		std::cout << std::endl;
+	}
+}
 #endif
